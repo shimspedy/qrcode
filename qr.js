9f845d80755117d0e721e85ee79ceba76108657d
john <36556216+shimspedy@users.noreply.github.com>
2024-01-27 22:21:19 -0800

    home

--- qr.js
+++ qr.js
@@ -6,4 +6,17 @@ function qr() {
       "image"
     ).innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
   }
-  
+  const ranger = document.querySelector("#speed");
+const speedUpdate = () => {
+  document.documentElement.style.setProperty("--speed", `${ranger.value}s`);
+};
+ranger.addEventListener("input", speedUpdate);
+
+// Create function qr
+
+function qr() {
+  var t = document.getElementById("text").value;
+  document.getElementById(
+    "image"
+  ).innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
+}
