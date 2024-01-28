41ce0f5d3f372195d79a73ee17508918ce1c7a82
john <36556216+shimspedy@users.noreply.github.com>
2024-01-28 01:10:07 -0800

    home

--- qr.js
+++ qr.js
@@ -1,12 +1,4 @@
-// Create function qr
-
-function qr() {
-    var t = document.getElementById("text").value;
-    document.getElementById(
-      "image"
-    ).innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
-  }
-  const ranger = document.querySelector("#speed");
+const ranger = document.querySelector("#speed");
 const speedUpdate = () => {
   document.documentElement.style.setProperty("--speed", `${ranger.value}s`);
 };
@@ -20,3 +12,25 @@ function qr() {
     "image"
   ).innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
 }
+
+// Create function barcode
+JsBarcode("#barcode", "123456789");
+
+const entry = document.querySelector("#entry");
+const printButton = document.querySelector("#print-button");
+
+entry.focus();
+
+let text = "";
+
+entry.addEventListener("keyup", (e) => {
+  if (!e.target.value) {
+    JsBarcode("#barcode", "123456789");
+  }
+  text = e.target.value;
+  JsBarcode("#barcode", text);
+});
+
+printButton.addEventListener("click", () => {
+  printJS("barcode", "html");
+});
